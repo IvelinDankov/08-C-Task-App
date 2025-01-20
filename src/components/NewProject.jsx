5464ae7d0e7cd9c47cd66680b62547713a88c20a
Velin Dankov <velin80@web.de>
2025-01-20 14:24:55 +0200

    creating Modal for errorhandling

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,10 @@
 import { useRef } from 'react';
 import Input from './Input.jsx';
+import Modal from './Modal.jsx';
 
 export default function NewProject({ onAdd }) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -13,6 +16,15 @@ export default function NewProject({ onAdd }) {
 
     // error Handling
 
+    if (
+      enteredTitle.trim() === '' ||
+      enteredDescription.trim() === '' ||
+      enteredDueDate.trim() === ''
+    ) {
+      modal.current.open();
+      return;
+    }
+
     onAdd({
       title: enteredTitle,
       description: enteredDescription,
@@ -21,36 +33,42 @@ export default function NewProject({ onAdd }) {
   }
 
   return (
-    <div className="w-[35rem] mt-16 ">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={handleSave}
-            className="py-2 px-6 bg-stone-800 text-stone-50 hover:bg-stone-950 rounded-md">
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input
-          ref={title}
-          label="title"
-        />
-        <Input
-          ref={description}
-          label="description"
-        />
-        <Input
-          type="date"
-          ref={dueDate}
-          label="dueDate"
-        />
+    <>
+      <Modal ref={modal}>
+        <h2 className="uppercase">This is Error Message</h2>
+        <p>Oops.. looks like you forgot to </p>
+      </Modal>
+      <div className="w-[35rem] mt-16 ">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleSave}
+              className="py-2 px-6 bg-stone-800 text-stone-50 hover:bg-stone-950 rounded-md">
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input
+            ref={title}
+            label="title"
+          />
+          <Input
+            ref={description}
+            label="description"
+          />
+          <Input
+            type="date"
+            ref={dueDate}
+            label="dueDate"
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
